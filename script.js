a52312ab36b420bd607acdadf8d957e079b497a8
marycynthiaamarachukwu8@gmail.com <marycynthiaamarachukwu8@gmail.com>
2025-04-11 14:43:08 +0100

    inital commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let secretNumber = Math.floor(Math.random()*100)+ 1; let attempts = 0;
+
+function checkGuess() {
+    let userGuess = document.getElementById("guess").value;
+    let message =
+    document.getElementById("message");
+    let attemptsDisplay =
+    document.getElementById("attempts");
+
+    attempts++;
+    attemptsDisplay.innerText = attempts;
+
+    if (userGuess < secretNumber) {
+        message.innerText = "Too low! Try again"
+        message.style.color = "red";
+    }else if (userGuess > secretNumber){
+        message.innerText = "Too high!Try again";
+        message.style.color = "red";
+    } else {
+        message.innerText = "congratulation! you guessed it in $ {attempts} attempts.;"
+        message.style.color = "green";
+    }
+
+}
+function resetGame() {
+    secretNumber =Math.floor(Math.random()* 100) + 1;
+    attempts = 0;
+
+    document.getElementById("guess").value = "";
+
+    document.getElementById("message").innerText = "";
+
+    document.getElementById("attempts").innerText = attempts
+
+}
